00dd33bfcec315b55e9968943c717ee1768d8727
Matt Knouff <matt.knouff@mkdesignsoftware.com>
2015-05-14 01:44:25 -0400

    2nd commit - learning sprite kit with book and using examples

--- Crazy Space/GameScene.swift
+++ Crazy Space/GameScene.swift
@@ -5,6 +5,10 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let backgroundNode : SKSpriteNode?
+    var playerNode : SKSpriteNode?
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
@@ -12,5 +16,15 @@ class GameScene: SKScene {
     override init(size:CGSize) {
         super.init(size: size)
         backgroundColor = SKColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        
+        backgroundNode  = SKSpriteNode(imageNamed: "Background")
+        backgroundNode!.size.width = self.frame.size.width
+        backgroundNode!.anchorPoint = CGPoint(x: 0.5, y:0.0)
+        backgroundNode!.position = CGPoint(x: size.width / 2.0, y:0.0)
+        addChild(backgroundNode!)
+        
+        playerNode = SKSpriteNode(imageNamed: "Player")
+        playerNode!.position = CGPoint(x: size.width / 2.0, y: 80.0)
+        addChild(playerNode!)
     }
 }
