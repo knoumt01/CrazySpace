e548c1769389a424fb5a6ea39014741bddaef9d8
Matt Knouff <matt.knouff@mkdesignsoftware.com>
2015-05-13 20:03:54 -0400

    An initial commit for crazy space.

--- Crazy Space/GameScene.swift
+++ Crazy Space/GameScene.swift
@@ -1,45 +1,16 @@
-//
-//  GameScene.swift
-//  Crazy Space
-//
-//  Created by Megan Hunter on 5/13/15.
-//  Copyright (c) 2015 MK Design Software. All rights reserved.
-//
+// GameScene.swift
+// part of Crazy Space, the first iOS game developed by MK
+// (c) 2015 All Rights Reserved
 
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
-        
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
-    }
-   
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
+    override init(size:CGSize) {
+        super.init(size: size)
+        backgroundColor = SKColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
     }
 }
